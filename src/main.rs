9ea9aedbff74d2e8547ebb988c5d214650e89ff7
dbisk <16860792+dbisk@users.noreply.github.com>
2019-08-23 15:12:45 -0700

    added preliminary testing structure

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 #![no_std] // no standard library
 #![no_main] // no main function (program-level entry point)
 
+// some attributes to allow for a custom test framework
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+// change the auto-generated tests function to something callable (not "main")
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -11,6 +18,10 @@ pub extern "C" fn _start() -> ! {
 
     vga_buffer::vga_text_mode_test(vga_buffer::Color::Yellow, vga_buffer::Color::Black);
 
+    // run tests if we are in test configuration
+    #![cfg(test)]
+    test_main();
+
     // loop indefinitely
     loop {}
 }
@@ -19,5 +30,14 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
     loop {}
+}
+
+// runs all the functions in the passed in array of test functions
+#![cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Starting first of {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
 
